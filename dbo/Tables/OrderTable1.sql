8e649e861c85640e0de4314fab2cf9f42803990c
Olugbenga <aojgbenga@gmail.com>
2024-02-07 14:59:02 +0000

    Added Order Table

---
+++ dbo/Tables/OrderTable1.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[OrderTable1] (
+    [ID]          INT            NULL,
+    [OrderName]   NVARCHAR (100) NULL,
+    [OrderNumber] INT            NULL,
+    [OrderDate]   DATE           NULL
+);
+
+
+GO
+CREATE CLUSTERED INDEX [IX_OrderTable1_ID]
+    ON [dbo].[OrderTable1]([ID] ASC);
+
